fce177ccc53925427aaf373c9061c42bf80c1e65
Morten Piibeleht <morten.piibeleht@gmail.com>
2016-08-05 16:17:36 +0300

    Move compat code to a separate file

--- lib/HTMLWriter.jl
+++ lib/HTMLWriter.jl
@@ -23,26 +23,7 @@ import Documenter.Documents: NavNode, navpath
 
 include("Pygments.jl")
 
-
-# Necessary until HTML PR is merged
-if isdefined(Documenter.Utilities, :MDFlatten)
-    using Documenter.Utilities.MDFlatten
-    import Documenter.Utilities.DOM: HTMLDocument
-else
-    include("MDFlatten.jl")
-    mdflatten = MDFlatten.mdflatten
-
-    type HTMLDocument
-        doctype::String
-        root::DOM.Node
-    end
-    HTMLDocument(root) = HTMLDocument("html", root)
-
-    function Base.show(io::IO, doc::HTMLDocument)
-        println(io, "<!DOCTYPE $(doc.doctype)>")
-        println(io, doc.root)
-    end
-end
+include("compat.jl")
 
 """
 Build a `Page` "database" out of a `Document`.
@@ -203,58 +184,6 @@ function render(::Writer{Formats.HTML}, doc::Documents.Document)
     println(fout_search, "]}")
     close(fout_search)
 end
-
-################################################################################
-################################### TO  COPY ###################################
-################################################################################
-
-"""
-Calculates a relative HTML link.
-"""
-function relhref(from, to)
-    pagedir = dirname(from)
-    relpath(to, isempty(pagedir) ? "." : pagedir)
-end
-
-"""
-Copies an asset from Documenters `assets/html/` directory to `doc.user.build`.
-Returns the path of the copied asset relative to `.build`.
-
-Note: while the current implementation trivially just appends `file` to
-`doc.user.build`, this behaviour should not be assumed and could change in the
-future. Instead the returned path should be used to figure out the links to the
-asset.
-"""
-function copy_asset(file, doc)
-    info(" - copying asset: $file")
-    src = joinpath("assets/", file) # TODO: Utilities.assetsdir()
-    dst = joinpath("build/", file)  # TODO: doc.user.build / assets
-    isfile(src) || error("Asset '$file' not found at $(abspath(src))")
-    ispath(dirname(dst)) || mkpath(dirname(dst))
-    cp(src, dst, remove_destination=true)
-    normpath(file)
-end
-
-"""
-Copies over a stylesheet from the assets directory and returns the `link` node.
-
-`stylesheet` should be just the name of the stylesheet with the extension
-(e.g. `normalize.css`). `src` is the path of the current page and used to
-determine the relative href.
-"""
-function stylesheet_asset(stylesheet, src)
-    asset = joinpath("css", stylesheet)
-    path = copy_asset(asset, src)
-    DOM.Tag(:link)[
-        :href => relhref(src, path),
-        :rel => "stylesheet",
-        :type => "text/css"
-    ]()
-end
-
-################################################################################
-################################################################################
-################################################################################
 
 _relpath(to, from) = relhref(from, to)
 
